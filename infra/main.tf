495b6fcde63db9bb9f4be59d2a0e8df95b207f39
Baptiste Dromer <baptiste.dromer@gmail.com>
2023-06-15 13:29:26 +0200

    Fix terraform fmt

--- infra/main.tf
+++ infra/main.tf
@@ -1,24 +1,24 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.3.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.3.0"
     }
+  }
 
-    required_version = "~> 1.5.0"
+  required_version = "~> 1.5.0"
 
-    backend "s3" {
-        bucket = "tfstate-bucket-bpt"
-        region = "eu-central-1"
-        key    = "dev"
-    }
+  backend "s3" {
+    bucket = "tfstate-bucket-bpt"
+    region = "eu-central-1"
+    key    = "dev"
+  }
 }
 
 provider "aws" {
-    region = "eu-central1"
+  region = "eu-central1"
 }
 
 resource "aws_s3_bucket" "frontbucket" {
-    bucket = "repo-analyzer-front"
+  bucket = "repo-analyzer-front"
 }
